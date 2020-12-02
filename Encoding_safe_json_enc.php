a71a03d927be8cc45915eec28350a7287758d1b5
Andrey Chekmarev <assminog@gmail.com>
2020-12-02 23:40:29 +0300

    Add files via upload

---
+++ Encoding_safe_json_enc.php
@@ -0,0 +1,19 @@
+<?php
+function strMyJson($arr)
+	{
+	$str	='';
+	if(is_array($arr))
+		{
+		$str	='{';
+		foreach($arr as $strName=>$strValue)
+			{
+			$strName	=str_replace('"','',$strName);
+		$strValue	=str_replace('"','',$strValue);
+			$str	.='"'.$strName.'":"'.$strValue.'",';
+			}
+		$str	=substr($str,0,-1);
+		$str	.='}';
+		}
+	return $str;
+	}
+?>
